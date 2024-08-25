ff312cbb304cbec2572de6ffc4c7aa6ed3e76a39
Kuzin <daniel@kuzin.dk>
2024-08-25 18:29:10 +0200

    Typescript support

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,7 +1,7 @@
 return {
   {
     "stevearc/conform.nvim",
-    -- event = 'BufWritePre', -- uncomment for format on save
+    event = 'BufWritePre', -- uncomment for format on save
     opts = require "configs.conform",
   },
 
@@ -12,7 +12,17 @@ return {
       require "configs.lspconfig"
     end,
   },
-
+  {
+    "williamboman/mason.nvim",
+    opts = {
+       ensure_installed = {
+          "prettierd",
+          "eslint-lsp",
+          "tailwindcss-language-server",
+          "typescript-language-server",
+      }
+    }
+  }
   -- {
   -- 	"nvim-treesitter/nvim-treesitter",
   -- 	opts = {
