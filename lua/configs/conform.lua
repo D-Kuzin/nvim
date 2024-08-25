ce9001e5d65e26c947f04b11509ad9d3e7ecbac6
Kuzin <daniel@kuzin.dk>
2024-08-25 19:03:09 +0200

    format

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -3,11 +3,11 @@ local options = {
     lua = { "stylua" },
     css = { "prettierd" },
     html = { "prettierd" },
-    javascript = { "prettierd"}
+    javascript = { "prettierd" },
   },
 
   format_on_save = {
-  -- These options will be passed to conform.format()
+    -- These options will be passed to conform.format()
     timeout_ms = 500,
     lsp_fallback = true,
   },
